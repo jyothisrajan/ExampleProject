18b9b1d6df6c36aa819ce53a0a41157b8e7adba2
jyothisrajan <jyothis@jyr.com>
2019-09-05 11:20:35 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("my github example project");
+		//take input from keyboard
+		System.out.println("What's your name?");
+		Scanner keyboard = new Scanner(System.in);
+		String personName = keyboard.nextLine();
+		System.out.println("Hello "+ personName);
+		
+		
+
+	}
+
+}
